12bc56ab08fffc534485f350b1263a322d44c2b7
Kushol Roy <89259123+kroxy2k@users.noreply.github.com>
2024-11-27 17:19:12 +0530

    Update prog12.py

--- input_output/prog12.py
+++ input_output/prog12.py
@@ -1,8 +1,7 @@
 '''If the total selling price of 15 items and the total profit earned on them is input through the
 keyboard, write a program to find the cost price of one item.'''
-# Taking input from the user
+
 TsellPr=int(input("Enter total selling price of 15 items:"))
 Tprofit=int(input("Enter total profit earned on 15 items:"))
-# Calculating the cost price of one item
 CP=(Tprofit/(15))+(TsellPr/(15))
 print("Cost price of one item is:",CP)
